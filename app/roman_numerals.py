284ba037001d13a045cbc3ebde66c19d992eb0c9
Daniel TIllero <danieltillero67@gmail.com>
2020-12-06 01:32:22 -0500

    refactor parse

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,15 +1,22 @@
+# def parse(roman):
+#     if roman == "I":
+#         return 1
+#     elif roman == "II":
+#         return 2
+#     elif roman == "III":
+#         return 3
+#     elif roman == "IV":
+#         return 4
+#     elif roman == "V":
+#         return 5
+#     elif roman == "VI":
+#         return 6
+#     elif roman == "VII":
+#         return 7
+#     else:
+#         return 8
+
 def parse(roman):
-    if roman == "I":
-        return 1
-    elif roman == "II":
-        return 2
-    elif roman == "III":
-        return 3
-    elif roman == "IV":
-        return 4
-    elif roman == "V":
-        return 5
-    elif roman == "VI":
-        return 6
-    else:
-        return 7
+    roman_dict = {"I": 1, "II": 2, "III": 3, "IV": 4,
+                  "V": 5, "VI": 6, "VII": 7, "VIII": 8}
+    return roman_dict[roman]
